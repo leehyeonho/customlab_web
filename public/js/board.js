b5868d46550cbfd910e9f99d4b2d70e33762d8b7
leehh <newlhh2@naver.com>
2020-10-09 13:51:42 +0900

    201009

--- public/js/board.js
+++ public/js/board.js
@@ -70,13 +70,11 @@ exports.upload = function(request, response) {
   var title = request.body.title;
   var content = request.body.content;
   sql = 'INSERT INTO bbs_gallery(author, title, content, img) values (?, ?, ?, ?)';
-  db.query(sql, [author, title, content, request.file.path.substring(6)], function(error, result) {
+  db.query(sql, [author, title, content, request.file[0].path.substring(6)], function(error, result) {
     if(error) {
       console.log(error);
     }else {
       console.log("post")
-      console.log(request.file)
-      console.log(request.file.path.substring(6))
   	  response.redirect('/board.ejs?tbl=2&pageNum=1');
   	}
     });
