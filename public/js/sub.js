63e9ccc300e63302410ced7be6d92ccc2c541691
dlgusgh0812 <tkfkd08gkrl12>
2021-02-15 16:02:19 +0900

    update

--- public/js/sub.js
+++ public/js/sub.js
@@ -28,12 +28,16 @@ exports.view = function(request, response) {
             $bodyList.each(function(i, elem) {
               ulList[i] = {
                   title: $(this).find('a._3BkKgDHq3l').text()
-                  // image_alt: $(this).find('p.poto a img').attr('alt'),
-                  // summary: $(this).find('p.lead').text().slice(0, -11),
-                  // date: $(this).find('span.p-time').text()
+                  url: $(this).find('a._3BkKgDHq3l').attr('href'),
+                  image_url: $(this).find('img._25CKxIKjAk').attr('src'),
+                  image_alt: $(this).find('img._25CKxIKjAk').attr('alt'),
+                  price: $(this).find('div._23DThs7PLJ span._45HSXeff1y span.nIAdxeTzhx').text(),
+                  sale: $(this).find('div._23DThs7PLJ strong span.nIAdxeTzhx').text(),
+                  percent: $(this).find('strong.pT4bw14aV2').text()
               };
             });
             const data = ulList.filter(n => n.title);
+            console.log(data);
             return data;
           })
           .then(res => response.render('sub', {session : request.session, data : result, naver : res, tbl : tbl}));
