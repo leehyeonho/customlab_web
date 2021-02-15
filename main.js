fede39e89480ece8a240600faf0fed60ad8aeb1b
dlgusgh0812 <tkfkd08gkrl12>
2021-02-15 17:56:59 +0900

    update

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const KakaoStrategy = require('passport-kakao').Strategy;
 
 passport.use('kakao', new KakaoStrategy({
     clientID: '4aaf1a669526ce81793050bf7267a81c',
-    callbackURL: 'http://customlab.site/oauth',     // 위에서 설정한 Redirect URI
+    callbackURL: '/auth/kakao/callback',     // 위에서 설정한 Redirect URI
   }, async (accessToken, refreshToken, profile, done) => {
     //console.log(profile);
     console.log(accessToken);
