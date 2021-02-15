4460c3ac6ede7326080a74338b2bca2ab9ae2ee6
dlgusgh0812 <tkfkd08gkrl12>
2021-02-15 15:36:26 +0900

    update

--- main.js
+++ main.js
@@ -117,7 +117,6 @@ getHtml()
     let ulList = [];
     const $ = cheerio.load(html.data);
     const $bodyList = $("ul.wOWfwtMC_3._3cLKMqI7mI._7iBijuoyJl").children("li.qHwcFXhj0");
-    colsole.log($bodyList);
     $bodyList.each(function(i, elem) {
       ulList[i] = {
           title: $(this).find('a._3BkKgDHq3l').attr('href')
