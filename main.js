366ad69dbcdb5de6caa109511c349670178164f9
dlgusgh0812 <tkfkd08gkrl12>
2021-02-16 13:51:07 +0900

    update

--- main.js
+++ main.js
@@ -148,7 +148,7 @@ app.get(
   passport.authenticate('kakao', {
     failureRedirect: "/sub.ejs?tbl=info"
   }), (req, res) => {
-    console.log(req.user.profile);
+    console.log(req.user);
     // request.session.user_id = result[0].user_id;
  		// request.session.user_name = result[0].user_name;
  		// request.session.user_tell = result[0].user_tell;
