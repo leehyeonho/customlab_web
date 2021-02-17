de8ba7539a76c006deff88214e3f812ccbd2bb28
dlgusgh0812 <tkfkd08gkrl12>
2021-02-17 10:42:10 +0900

    update

--- main.js
+++ main.js
@@ -143,7 +143,7 @@ app.get(
 function kakaoPay() {
   var headers = {
     'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-    'Authorization': 'Bearer ' + access_token,
+    'Authorization': 'KakaoAK b980142b0d3b61c70c01646deb852459',
     cid : 'TC0ONETIME',
     partner_order_id : 'partner_order_id',
     partner_user_id : 'partner_user_id',
